1cbab4f75f7fee5cb10c4bd4909fbbf7eabef421
Q-Minh <tonthat.quocminh@gmail.com>
2019-09-05 13:36:52 -0400

    commit before doing major refactoring to use boost.asio instead of asio since there is no compat with asio and boost.beast

--- include/tcp/cluster_discovery_service.hpp
+++ include/tcp/cluster_discovery_service.hpp
@@ -8,9 +8,20 @@
 #include <tuple>
 #include <random>
 
+#include <boost/beast/core/tcp_stream.hpp>
+#include <boost/beast/http/dynamic_body.hpp>
+#include <boost/beast/core/flat_buffer.hpp>
+#include <boost/beast/http/write.hpp>
+#include <boost/beast/http/read.hpp>
+#include <boost/beast/http/string_body.hpp>
+#include <boost/beast/http/message.hpp>
+#include <boost/beast/http/status.hpp>
+
 #include <asio/io_context.hpp>
 #include <asio/execution_context.hpp>
 #include <asio/ip/tcp.hpp>
+
+#include <nlohmann/json.hpp>
 
 #include "cluster_settings.hpp"
 #include "message/cluster_messages.hpp"
@@ -32,7 +43,8 @@ public:
 		asio::execution_context& ioc
 	) : asio::execution_context::service(ioc),
 		settings_(),
-		old_gossip_()
+		old_gossip_(),
+		context_(nullptr)
 	{}
 
 	explicit cluster_discovery_service(
@@ -41,8 +53,57 @@ public:
 	)
 		: asio::execution_context::service(ioc),
 		settings_(settings),
-		old_gossip_()
+		old_gossip_(),
+		context_(nullptr)
 	{}
+
+	template <class Connection, class Func>
+	bool do_discover_node_endpoints(Connection& connection, Func&& f, int attempt)
+	{
+		if (attempt <= settings_.max_discover_attempts())
+		{
+			std::optional<node_endpoints_type> endpoints = discover_endpoints();
+			if (endpoints.has_value())
+			{
+				// for now, we'll just use .first to use the normal tcp endpoint instead of the secure one (ssl not yet used)
+				f(std::error_code(), endpoints.value().first);
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		return false;
+	}
+
+	template <class Connection, class Func>
+	void perform_discovery(Connection& connection, Func&& f, std::shared_ptr<asio::steady_timer> timer, int attempt)
+	{
+		asio::post(
+			connection.get_io_context(),
+			[this, timer = timer, &connection, f = std::move(f), attempt = attempt]()
+		{
+			if (!this->do_discover_node_endpoints(connection, std::move(f), attempt))
+			{
+				timer->expires_after(std::chrono::milliseconds(500));
+				timer->async_wait(
+					[this, timer = timer, &connection, f = std::move(f), attempt = attempt + 1 /*here is where we increment the attempt, as in the for loop version of the .net client*/]
+				(std::error_code ec)
+				{
+					if (!ec)
+					{
+						this->perform_discovery(connection, std::move(f), timer, attempt);
+					}
+					else
+					{
+						// do something
+					}
+				});
+			}
+		});
+	}
 
 	template <class Connection, class Func>
 	void async_discover_node_endpoints(Connection& connection, Func&& f)
@@ -52,29 +113,15 @@ public:
 			"template argument to Func must have signature void(asio::error_code, endpoint_type)"
 			);
 
-		auto& ioc = connection.get_io_context();
-
-		asio::post(
-			ioc,
-			[this, f = std::move(f), &ioc]()
-		{
-			for (int attempt = 1; attempt <= settings_.max_discover_attempts(); ++attempt)
-			{
-				std::optional<node_endpoints_type> endpoints = discover_endpoints();
-				if (endpoints.has_value())
-				{
-					// for now, we'll just use .first to use the normal tcp endpoint instead of the secure one (ssl not yet used)
-					f(std::make_error_code(), endpoints.value().first);
-				}
-
-				// sleep for 500 ms, we should use a timer here instead
-			}
-		});
+		context_ = std::addressof(connection.get_io_context());
+		auto timer = std::make_shared<asio::steady_timer>();
+		// start the discovery cycle
+		this->perform_discovery(connection, std::forward<Func>(f), timer, 1);
 	}
 
 	std::optional<node_endpoints_type> discover_endpoints()
 	{
-		std::vector<tcp::gossip_seed> candidates = old_gossip_.empty() ? get_dns_candidates() : 
+		std::vector<tcp::gossip_seed> candidates = old_gossip_.empty() ? get_dns_candidates() :
 			get_candidates(old_gossip_);
 
 		for (int i = 0; i < candidates.size(); ++i)
@@ -112,9 +159,7 @@ public:
 
 	std::vector<tcp::gossip_seed> get_candidates(std::vector<message::member_info> const& old_gossip)
 	{
-		
-
-		
+		return arrange_gossip_candidates(old_gossip);
 	}
 
 	std::vector<tcp::gossip_seed> arrange_gossip_candidates(std::vector<message::member_info> const& members)
@@ -150,11 +195,26 @@ public:
 		std::shuffle(result.begin() + j, result.end(), gen);
 
 		delete[] buffer;
+
+		return result;
 	}
 
 	auto try_get_gossip_from(tcp::gossip_seed const& seed) 
 		-> std::optional<message::cluster_info>
 	{
+		std::ostringstream oss{};
+		oss << "http://"
+			<< seed.endpoint()
+			<< "/gossip?format=json";
+		std::string url = oss.str();
+
+		namespace beast = boost::beast;
+		beast::tcp_stream stream{ *context_ };
+
+		std::error_code ec;
+		
+		//stream.connect(seed.endpoint());
+
 
 	}
 
@@ -241,6 +301,7 @@ private:
 
 	cluster_settings settings_;
 	std::vector<message::member_info> old_gossip_;
+	asio::io_context* context_;
 };
 
 } // services
